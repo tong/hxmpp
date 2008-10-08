31cede5f05f4fc120d93d6e02328e7b4be63eae4
tong <tong@disktree.net>
2008-10-08 22:08:31 +0200

    added DelayedDeliver ( XEP 0091, XEP 0203 ).

---
+++ jabber/DelayedDelivery.hx
@@ -0,0 +1,56 @@
+package jabber;
+
+
+typedef PacketDelay = {
+	
+	/**
+		The Jabber ID of the entity that originally sent the XML stanza
+		or that delayed the delivery of the stanza (e.g., the address of a multi-user chat room).
+	*/
+	var from : String;
+	
+	/**
+		The time when the XML stanza was originally sent.
+	*/
+	var stamp : String;
+	
+	/**
+		Description of the reason for the delay.
+	*/
+	var description : String;
+	
+}
+
+
+/**
+	<a href="http://xmpp.org/extensions/xep-0203.html">XEP-0203: Delayed Delivery</a><br/>
+	Use compiler flag 'XEP_091' for backwards compatibility with <a href="http://xmpp.org/extensions/xep-0091.html">XEP-0091: Delayed Delivery</a><br/>.
+*/
+class DelayedDelivery {
+	
+	public static var XMLNS = "urn:xmpp:delay";
+	
+	/**
+		Parses/returns the delay extension of this xmpp packet.
+	*/
+	public static function getDelay( m : xmpp.Message ) : PacketDelay {
+		for( e in m.properties ) {
+			if( e.nodeName == "delay" && e.get( "xmlns" ) == XMLNS ) {
+				return { from : e.get( "from" ),
+						 stamp : e.get( "stamp" ),
+						 description : e.firstChild().nodeValue };
+			}
+			#if XEP_091
+			if( e.nodeName == "x" && e.get( "xmlns" ) == "jabber:x:delay" ) {
+				var description : String = null;
+				try { description = e.firstChild().nodeValue; } catch( e : Dynamic ) {}
+				return { from : e.get( "from" ),
+						 stamp : e.get( "stamp" ),
+						 description : description };
+			}
+			#end
+		}
+		return null;	
+	}
+	
+}
