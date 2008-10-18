c92b91e950e5482f048f5ff166c6e7bfc91dd1c2
tong <tong@disktree.net>
2008-10-18 02:36:19 +0200

    added xmpp DataForma nd test with example from xmpp.org.

--- test/TestXMPPIQExtensions.hx
+++ test/TestXMPPIQExtensions.hx
@@ -1,5 +1,6 @@
 
 import xmpp.Roster;
+import xmpp.DataForm;
 
 
 
@@ -17,6 +18,7 @@ class TestXMPPIQExtensions {
 		var r = new haxe.unit.TestRunner();
 		r.add( new TestAuthExtension() );
 		r.add( new TestRosterExtension() );
+		r.add( new TestDataFormExtension() );
 		r.run();
 	}
 	
@@ -24,7 +26,7 @@ class TestXMPPIQExtensions {
 
 
 
-class TestAuthExtension extends haxe.unit.TestCase   {
+class TestAuthExtension extends haxe.unit.TestCase {
 	
 	public function testAuthExt() {
 		var iq = xmpp.IQ.parse( Xml.parse( '<iq type="set" id="66ceE3"><query xmlns="jabber:iq:auth"><username>tong</username><password>test</password><resource>norc</resource><digest>123</digest></query></iq>' ).firstElement() );
@@ -39,7 +41,7 @@ class TestAuthExtension extends haxe.unit.TestCase   {
 
 
 
-class TestRosterExtension extends haxe.unit.TestCase   {
+class TestRosterExtension extends haxe.unit.TestCase {
 	
 	public function testRosterExt() {
 		//TODO
@@ -66,6 +68,142 @@ class TestRosterExtension extends haxe.unit.TestCase   {
 	//	ext.subscription = xmpp.Subscription.both;
 	//.........TODO
 		*/
+	}
+
+}
+
+
+
+class TestDataFormExtension extends haxe.unit.TestCase {
+	
+	public function testAuthExt() {
+		
+		var form = xmpp.DataForm.parse( Xml.parse( "<x xmlns='jabber:x:data' type='form'>
+      <title>Bot Configuration</title>
+      <instructions>Fill out this form to configure your new bot!</instructions>
+      <field type='hidden'
+             var='FORM_TYPE'>
+        <value>jabber:bot</value>
+      </field>
+      <field type='fixed'><value>Section 1: Bot Info</value></field>
+      <field type='text-single'
+             label='The name of your bot'
+             var='botname'/>
+      <field type='text-multi'
+             label='Helpful description of your bot'
+             var='description'/>
+      <field type='boolean'
+             label='Public bot?'
+             var='public'>
+        <required/>
+      </field>
+      <field type='text-private'
+             label='Password for special access'
+             var='password'/>
+      <field type='fixed'><value>Section 2: Features</value></field>
+      <field type='list-multi'
+             label='What features will the bot support?'
+             var='features'>
+        <option label='Contests'><value>contests</value></option>
+        <option label='News'><value>news</value></option>
+        <option label='Polls'><value>polls</value></option>
+        <option label='Reminders'><value>reminders</value></option>
+        <option label='Search'><value>search</value></option>
+        <value>news</value>
+        <value>search</value>
+      </field>
+      <field type='fixed'><value>Section 3: Subscriber List</value></field>
+      <field type='list-single'
+             label='Maximum number of subscribers'
+             var='maxsubs'>
+        <value>20</value>
+        <option label='10'><value>10</value></option>
+        <option label='20'><value>20</value></option>
+        <option label='30'><value>30</value></option>
+        <option label='50'><value>50</value></option>
+        <option label='100'><value>100</value></option>
+        <option label='None'><value>none</value></option>
+      </field>
+      <field type='fixed'><value>Section 4: Invitations</value></field>
+      <field type='jid-multi'
+             label='People to invite'
+             var='invitelist'>
+        <desc>Tell all your friends about your new bot!</desc>
+      </field>
+    </x>" ).firstElement() );
+    	
+
+		assertEquals( 'Bot Configuration', form.title );
+		assertEquals( 'Fill out this form to configure your new bot!', form.instructions );
+		assertEquals( DataFormFieldType.hidden, form.fields[0].type );
+		assertEquals( 'FORM_TYPE', form.fields[0].variable );
+		assertEquals( 'jabber:bot', form.fields[0].values[0] );
+		
+		assertEquals( DataFormFieldType.fixed, form.fields[1].type );
+		assertEquals( 'Section 1: Bot Info', form.fields[1].values[0] );
+		
+		assertEquals( DataFormFieldType.text_single, form.fields[2].type );
+		assertEquals( 'botname', form.fields[2].variable );
+		assertEquals( 'The name of your bot', form.fields[2].label );
+		
+		assertEquals( DataFormFieldType.text_multi, form.fields[3].type );
+		assertEquals( 'Helpful description of your bot', form.fields[3].label );
+		assertEquals( 'description', form.fields[3].variable );
+		
+		assertEquals( DataFormFieldType.boolean, form.fields[4].type );
+		assertEquals( 'Public bot?', form.fields[4].label );
+		assertEquals( 'public', form.fields[4].variable );
+		
+		assertEquals( DataFormFieldType.text_private, form.fields[5].type );
+		assertEquals( 'Password for special access', form.fields[5].label );
+		assertEquals( 'password', form.fields[5].variable );
+		
+		assertEquals( DataFormFieldType.fixed, form.fields[6].type );
+		assertEquals( 'Section 2: Features', form.fields[6].values[0] );
+		
+		assertEquals( DataFormFieldType.list_multi, form.fields[7].type );
+		assertEquals( 'What features will the bot support?', form.fields[7].label );
+		assertEquals( 'features', form.fields[7].variable );
+		assertEquals( 'Contests', form.fields[7].options[0].label );
+		assertEquals( 'contests', form.fields[7].options[0].value );
+		assertEquals( 'News', form.fields[7].options[1].label );
+		assertEquals( 'news', form.fields[7].options[1].value );
+		assertEquals( 'Polls', form.fields[7].options[2].label );
+		assertEquals( 'polls', form.fields[7].options[2].value );
+		assertEquals( 'Reminders', form.fields[7].options[3].label );
+		assertEquals( 'reminders', form.fields[7].options[3].value );
+		assertEquals( 'Search', form.fields[7].options[4].label );
+		assertEquals( 'search', form.fields[7].options[4].value );
+		assertEquals( 'news', form.fields[7].values[0] );
+		assertEquals( 'search', form.fields[7].values[1] );
+		
+		assertEquals( DataFormFieldType.fixed, form.fields[8].type );
+		assertEquals( 'Section 3: Subscriber List', form.fields[8].values[0] );
+		
+		assertEquals( DataFormFieldType.list_single, form.fields[9].type );
+		assertEquals( 'Maximum number of subscribers', form.fields[9].label );
+		assertEquals( 'maxsubs', form.fields[9].variable );
+		assertEquals( '20', form.fields[9].values[0] );
+		assertEquals( '10', form.fields[9].options[0].label );
+		assertEquals( '10', form.fields[9].options[0].value );
+		assertEquals( '20', form.fields[9].options[1].label );
+		assertEquals( '20', form.fields[9].options[1].value );
+		assertEquals( '30', form.fields[9].options[2].label );
+		assertEquals( '30', form.fields[9].options[2].value );
+		assertEquals( '50', form.fields[9].options[3].label );
+		assertEquals( '50', form.fields[9].options[3].value );
+		assertEquals( '100', form.fields[9].options[4].label );
+		assertEquals( '100', form.fields[9].options[4].value );
+		assertEquals( 'None', form.fields[9].options[5].label );
+		assertEquals( 'none', form.fields[9].options[5].value );
+		
+		assertEquals( DataFormFieldType.fixed, form.fields[10].type );
+		assertEquals( 'Section 4: Invitations', form.fields[10].values[0] );
+		
+		assertEquals( DataFormFieldType.jid_multi, form.fields[11].type );
+		assertEquals( 'People to invite', form.fields[11].label );
+		assertEquals( 'invitelist', form.fields[11].variable );
+		assertEquals( 'Tell all your friends about your new bot!', form.fields[11].desc );
 	}
 
 }
---
+++ xmpp/DataForm.hx
@@ -0,0 +1,204 @@
+package xmpp;
+
+import util.XmlUtil;
+
+
+enum DataFormType {
+	cancel;
+	form;
+	result;
+	submit;
+}
+
+
+enum DataFormFieldType {
+	boolean;
+	fixed;
+	hidden;
+	jid_multi;
+	jid_single;
+	list_multi;
+	list_single;
+	text_multi;
+	text_private;
+	text_single;
+}
+
+
+class DataFormFieldOption {
+	
+	public var label : String;
+	public var value : String;
+	
+	public function new( ?label : String, ?value: String ) {
+		this.label = label;
+		this.value = value;
+	}
+	
+	public function toXml() : Xml {
+		var x = Xml.createElement( "option" );
+		if( label != null ) x.set( "label", label );
+		if( value != null ) x.addChild( XmlUtil.createElement( "value", value ) );
+		return x;
+	}
+	
+	public static function parse( x : Xml ) : DataFormFieldOption {
+		var option = new DataFormFieldOption();
+		option.label = x.get( "label" );
+		option.value = x.elements().next().firstChild().nodeValue;
+		return option;
+	}
+}
+
+
+class DataFormField {
+	
+	// dirty hack for the enums.
+	static inline var hack_enum_from = ~/-/;
+	static inline var hack_enum_to = ~/_/;
+	
+	public var label : String;
+	public var type : DataFormFieldType;
+	public var variable : String;
+	public var desc : String;
+//	public var required : String; // hm ? 
+	public var values : Array<String>;
+	public var options : Array<DataFormFieldOption>;
+	
+	public function new() {
+		values = new Array();
+		options = new Array();
+	}
+	
+	public function toXml() : Xml {
+		var x = Xml.createElement( "field" );
+		if( label != null ) x.set( "label", label );
+		if( type != null ) x.set( "type", hack_enum_to.replace( Type.enumConstructor( type ), "-" ) );
+		if( variable != null ) x.set( "var", variable );
+//		if( required != null ) x.addChild( XmlUtil.createElement( "required", required ) );
+		if( desc != null ) x.addChild( XmlUtil.createElement( "desc", desc ) );
+		for( value in values ) x.addChild( XmlUtil.createElement( "value", value ) );
+		for( option in options ) x.addChild( option.toXml() );
+		return x;
+	}
+	
+	public static function parse( x : Xml ) : DataFormField  {
+		var field = new DataFormField();
+		if( x.exists( "label" ) ) field.label = x.get( "label" );
+		if( x.exists( "type" ) ) field.type = Type.createEnum( DataFormFieldType, hack_enum_from.replace ( x.get( "type" ), "_" ) );
+		if( x.exists( "var" ) ) field.variable = x.get( "var" );
+		for( e in x.elements() ) {
+			switch( e.nodeName ) {
+				case "desc" : try { field.desc = e.firstChild().nodeValue; } catch( e : Dynamic ) {}
+//				case "required" : 
+				case "option" : field.options.push( DataFormFieldOption.parse( e ) );
+				case "value" : try { field.values.push( e.firstChild().nodeValue ); } catch( e : Dynamic ) {}
+			}
+		}
+		return field;
+	}
+}
+
+
+class DataFormItem {
+	
+	public var fields : Array<DataFormField>;
+	
+	public function new( ?fields : Array<DataFormField> ) {
+		this.fields = if( fields != null ) fields else new Array<DataFormField>();
+	}
+	
+	public function toXml() : Xml {
+		return createXml( "item" );
+	}
+	
+	inline function createXml( n : String ) : Xml {
+		var x = Xml.createElement( n );
+		for( f in fields ) x.addChild( f.toXml() );
+		return x;
+	}
+	
+	public static function parse( x : Xml ) : DataFormItem {
+		return parseFields( new DataFormItem(), x );
+	}
+	
+	public static inline function parseFields( t : { fields : Array<DataFormField> }, x : Xml ) : untyped {} {
+		for( e in x.elementsNamed( "field" ) ) {
+			t.fields.push( DataFormField.parse( e.firstElement() ) );
+		}
+		return untyped t;
+	}
+}
+
+
+class DataFormReported extends DataFormItem {
+	
+	public function new( ?fields : Array<DataFormField> ) {
+		super( fields );
+	}
+	
+	public override function toXml() : Xml {
+		return createXml( "reported" );
+	}
+	
+	public static function parse( x : Xml ) : DataFormReported {
+		return DataFormItem.parseFields( new DataFormReported(), x );
+	}
+}
+
+
+/**
+	DataForm packet extension (for iq and message packets).
+*/
+class DataForm {
+	
+	public static var XMLNS = "jabber:x:data";
+	
+	public var type : DataFormType;
+	public var title : String;
+	public var instructions : String;
+	public var fields : Array<DataFormField>;
+	public var reported : DataFormReported;
+	public var items : Array<DataFormItem>;
+	
+	
+	public function new() {
+		fields = new Array();
+		items = new Array();
+	}
+	
+	
+	public function toXml() : Xml {
+		//TODO validate
+		var x = Xml.createElement( "x" );
+		x.set( "xmlns", XMLNS );
+		x.set( "type", Type.enumConstructor( type ) );
+		if( title != null ) x.addChild( XmlUtil.createElement( "title", title ) );
+		if( instructions != null ) x.addChild( XmlUtil.createElement( "instructions", instructions ) );
+		for( f in fields ) x.addChild( f.toXml() );
+		if( reported != null ) x.addChild( reported.toXml() );
+		for( i in items ) x.addChild( i.toXml() ); 
+		return x;
+	}
+	
+	public inline function toString() : String {
+		return toXml().toString();
+	}
+	
+	
+	public static function parse( x : Xml ) : DataForm {
+		
+		var f = new haxe.xml.Fast( x );
+		if( !f.has.xmlns || !f.has.type ) return null;
+		
+		var form = new DataForm();
+		form.type = Type.createEnum( DataFormType, f.att.type );
+		if( f.hasNode.title ) form.title = f.node.title.innerData;
+		if( f.hasNode.instructions ) form.instructions = f.node.instructions.innerData;
+		for( field in f.nodes.field ) form.fields.push( DataFormField.parse( field.x ) );
+		for( item in f.nodes.item ) form.items.push( DataFormItem.parse( item.x ) );
+		if( f.hasNode.reported ) form.reported = DataFormReported.parse( f.node.reported.x );
+		return form;
+	}
+	
+}
