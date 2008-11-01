85117f9f50b8b4ac10d9270c962a43b873207421
tong <tong@disktree.net>
2008-11-01 22:30:10 +0100

    chat fix.

--- jabber/Chat.hx
+++ jabber/Chat.hx
@@ -12,20 +12,20 @@ import xmpp.filter.PacketFromFilter;
 */
 class Chat {
 	
-	public dynamic function onMessage( chat : Chat ) {}
+	public dynamic function onMessage( c : Chat ) {}
 	
 	public var stream(default,null) : StreamBase;
 	public var peer(default,null) : String;
 	public var threadID(default,setThreadID) : String;
 	public var lastMessage(default,null) : xmpp.Message;
 	
-	var message : xmpp.Message;
-	var collector : PacketCollector;
+	var c : PacketCollector;
+	var m : xmpp.Message;
 	
 	
 	public function new( stream : StreamBase, myJid : String, peer : String, ?threadID : String ) {
 		
-		message = new xmpp.Message( xmpp.MessageType.chat, peer, null, null, threadID, myJid );
+		m = new xmpp.Message( xmpp.MessageType.chat, null, null, null, threadID, myJid );
 
 		this.stream = stream;
 		this.peer = peer;
@@ -33,13 +33,13 @@ class Chat {
 		
 		var mf : PacketFilter = new xmpp.filter.MessageFilter( xmpp.MessageType.chat );
 		var ff : PacketFilter = new xmpp.filter.PacketFromFilter( peer );
-		collector = new PacketCollector( [ mf, ff ], handleMessage, true );
-		stream.collectors.add( collector );
+		c = new PacketCollector( [ mf, ff ], handleMessage, true );
+		stream.collectors.add( c );
 	}
 	
 	
 	function setThreadID( id : String ) : String {
-		threadID = message.thread = id;
+		threadID = m.thread = id;
 		return id;
 	}
 	
@@ -48,20 +48,27 @@ class Chat {
 		Sends a chat message to the peer.
 	*/
 	public function speak( t : String ) : xmpp.Message {
-		message.body = t;
-		return stream.sendPacket( message );
+		m.body = t;
+		return stream.sendPacket( m );
 	}
 	
 	/**
+		Removes the collector from this stream.
 	*/
 	public function destroy() {
-		stream.collectors.remove( collector );
+		stream.collectors.remove( c );
 	}
 	
 	/**
 		Handles incoming message.
 	*/	
 	public function handleMessage( m : xmpp.Message ) {
+		#if JABBER_DEBUG
+		if( m.type != xmpp.MessageType.chat ) {
+			trace( "Chats can only handle chat-type messages" );
+			return;
+		}
+		#end;
 		lastMessage = m;
 		onMessage( this );
 	}
