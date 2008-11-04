b9fe651c65007b768e529a834b31f0d32c33fa06
tong <tong@disktree.net>
2008-11-04 03:35:34 +0100

    added xmpp XHTML support.

---
+++ xmpp/XHTML.hx
@@ -0,0 +1,47 @@
+package xmpp;
+
+
+/**
+	<a href="http://xmpp.org/extensions/xep-0071.html">XEP 0071 - XHTML-IM</a>
+*/
+class XHTML {
+	
+	public static var XMLNS = "http://jabber.org/protocol/xhtml-im";
+	
+	public var body : Xml;
+	
+	public function new() {}
+	
+	public function toXml() : Xml {
+		var x = Xml.createElement( "html" );
+		x.set( "xmlns", XMLNS );
+		x.addChild( body );
+		return x;
+	}
+	
+	/*
+	public static function parse( p : Xml ) : XHTML {
+	}
+	*/
+	
+	/**
+		Extracts the html tag from a message packet.
+	*/
+	public static function fromMessage( m : xmpp.Message ) : XHTML {
+		var xhtml = new XHTML();
+		for( p in m.properties ) {
+			if( p.nodeName == "html" && p.get( "xmlns" ) == "http://jabber.org/protocol/xhtml-im" ) {
+				var b = p.elements().next();
+				if( b != null ) {
+					if( b.nodeName == "body" && b.get( "xmlns" ) == "http://www.w3.org/1999/xhtml" ) {
+						xhtml.body = b;
+					}
+				} else {
+					xhtml = null;
+				}
+			}
+		}
+		return xhtml;
+	}	
+	
+}
