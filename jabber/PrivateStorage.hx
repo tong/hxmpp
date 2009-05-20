f7660c4c5ee639f621f5205c6619ae78a810bb5e
tong <tong@disktree.net>
2009-05-20 02:30:29 +0200

    added private storage support.

---
+++ jabber/PrivateStorage.hx
@@ -0,0 +1,52 @@
+package jabber;
+
+/**
+	Extension to store any arbitrary XML on the server side.
+	<a href="http://xmpp.org/extensions/xep-0049.html">XEP-0049: Private XML Storage</a><br/>
+*/
+class PrivateStorage {
+	
+	public dynamic function onStored( s : xmpp.PrivateStorage ) : Void;
+	public dynamic function onLoad( s : xmpp.PrivateStorage ) : Void;
+	public dynamic function onError( e : jabber.XMPPError ) : Void;
+	
+	public var stream(default,null) : jabber.Stream;
+
+	public function new( stream : jabber.client.Stream ) {
+		this.stream = stream;
+	}
+	
+	/**
+		Store private data.
+	*/
+	public function store( name : String, namespace : String, data : Xml ) {
+		var iq = new xmpp.IQ( xmpp.IQType.set );
+		var xt = new xmpp.PrivateStorage( name, namespace, data );
+		iq.ext = xt;
+		var me = this;
+		stream.sendIQ( iq, function(r:xmpp.IQ) {
+			switch( r.type ) {
+			case result : me.onStored( xt );
+			case error : me.onError( new jabber.XMPPError( me, iq ) );
+			default://#
+			}
+		} );
+	}
+	
+	/**
+		Load private data.
+	*/
+	public function load( name : String, namespace : String ) {
+		var iq = new xmpp.IQ( xmpp.IQType.get );
+		iq.ext = new xmpp.PrivateStorage( name, namespace );
+		var me = this;
+		stream.sendIQ( iq, function(r:xmpp.IQ) {
+			switch( r.type ) {
+			case result : me.onLoad( xmpp.PrivateStorage.parse( r.ext.toXml() ) );
+			case error : me.onError( new jabber.XMPPError( me, iq ) );
+			default://#
+			}
+		} );
+	}
+	
+}
---
+++ xmpp/PrivateStorage.hx
@@ -0,0 +1,31 @@
+package xmpp;
+
+class PrivateStorage {
+	
+	public static var XMLNS = "jabber:iq:private";
+	
+	public var name : String;
+	public var namespace : String;
+	public var data : Xml;
+	
+	public function new( name : String, namespace : String, ?data : Xml ) {
+		this.name = name;
+		this.namespace = namespace;
+		this.data = data;
+	}
+	
+	public function toXml() : Xml {
+		var x = xmpp.IQ.createQueryXml( XMLNS );
+		var e = Xml.createElement( name );
+		e.set( "xmlns", namespace );
+		if( data != null ) e.addChild( data );
+		x.addChild( e );
+		return x;
+	}
+	
+	public static function parse( x : Xml ) : PrivateStorage {
+		var e = x.firstChild();
+		return new PrivateStorage( e.nodeName, e.get("xmlns" ), e.firstElement() );
+	}
+	
+}
