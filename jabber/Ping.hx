c7e8d6e002e47d858f1a8e99c79a402f5a39fa4f
tong <tong@disktree.net>
2009-11-20 06:02:20 +0100

    set from field of outgoing ping iqs (required for server components).

--- jabber/Ping.hx
+++ jabber/Ping.hx
@@ -20,10 +20,10 @@ package jabber;
 import util.Timer;
 
 /**
-	flash,js,neko<br>
-	Sends application-level pings over XML streams.<br>
-	Such pings can be sent from a client to a server, from one server to another, or end-to-end.<br>
-	<a href="http://www.xmpp.org/extensions/xep-0199.html">XEP 199 - XMPP Ping</a>
+	<a href="http://www.xmpp.org/extensions/xep-0199.html">XEP 199 - XMPP Ping</a><br/>
+	<p>flash,js,neko</p>
+	Sends application-level pings over XML streams.<br/>
+	Such pings can be sent from a client to a server, from one server to another, or end-to-end.<br/>
 */
 class Ping {
 	
@@ -38,13 +38,14 @@ class Ping {
 	public var interval : Int;
 	/** The pinged target entity */
 	public var target : String;
+	/** Indicates if the ping interval is running */
 	public var active(default,null) : Bool;
 	
 	var timer : Timer;
 	
 	public function new( stream : Stream, ?target : String, ?interval : Int ) {
 		if( interval != null && interval <= 0 )
-			throw "Ping interval must be greater than 0";
+			throw "Invalid ping interval: "+interval;
 		this.target = target;
 		this.stream = stream;
 		this.interval = ( interval != null ) ? interval : defaultInterval;
@@ -74,7 +75,7 @@ class Ping {
 		Sends a ping packet to the given entity, or to the entities server if the to attribute is omitted.
 	*/
 	public function send( ?to : String ) {
-		var iq = new xmpp.IQ( null, null, to );
+		var iq = new xmpp.IQ( null, null, to, stream.jidstr );
 		iq.x = new xmpp.Ping();
 		var me = this;
 		var timeoutHandler = function( c : jabber.stream.PacketCollector ) {
