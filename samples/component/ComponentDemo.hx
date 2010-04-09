16bec2d8f28e30907d956594f3fd293ff2fdc35f
tong <tong@disktree.net>
2010-04-09 19:04:09 +0200

    removed the makefile, using a 'included' hxml for building the libs.

--- samples/component/ComponentDemo.hx
+++ samples/component/ComponentDemo.hx
@@ -3,19 +3,19 @@ import jabber.component.Stream;
 
 class ComponentDemo {
 	
-	static var cnx : jabber.SocketConnection;
 	static var stream : Stream;
 	
 	static function main() {
 		trace( "HXMPP server component example" );
 		var identity = { category : "conference", name : "MYSERVICE", type : "text" };
-		cnx = new jabber.SocketConnection( "127.0.0.1", Stream.defaultPort );
+		var cnx = new jabber.SocketConnection( "127.0.0.1", Stream.defaultPort );
 		stream = new Stream( "disktree", "norc", "1234", cnx, [identity] );
 		stream.onOpen = function() {
 			trace( "XMPP stream opened.", "info" );
-		};
-		//stream.onError = function(?m) { trace( "Stream error, "+m ); } ;
-		stream.onClose = function(?e) { trace( "Stream closed." ); } ;
+		}
+		stream.onClose = function(?e) {
+			trace( "Stream closed." );
+		}
 		stream.onConnect = function() {
 			trace( "Component connected. Have fun!", "info" );
 		}
