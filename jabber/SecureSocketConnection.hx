0b7e2f9155f0c6749b5cf44030096edb7add541f
tong <tong@disktree.net>
2010-05-11 01:05:05 +0200

    added a experimental SSL socket connection implementation for neko based on the hxssl project.

---
+++ jabber/SecureSocketConnection.hx
@@ -0,0 +1,100 @@
+package jabber;
+
+#if neko
+import neko.ssl.Socket;
+#elseif php
+import jabber.util.php.Socket;
+#end
+
+/**
+	Experimental SSL socket connection for neko.
+	Source files from the 'hxssl' project are required.
+*/
+class SecureSocketConnection extends jabber.stream.Connection {
+	
+	public static var defaultBufSize = 65536; //(1<<8);// 128
+	
+	public var port(default,null) : Int;
+	public var maxBufSize(default,null) : Int;
+	//public var timeout(default,null) : Int;
+	
+	var socket : Socket;
+	var reading : Bool;
+	var buf : haxe.io.Bytes;
+	var bufbytes : Int;
+	
+	public function new( host : String, port : Int = 5223,
+						 maxBufSize : Int = 65536,
+						 timeout : Int = 10 ) {
+		super( host );
+		this.port = port;
+		this.maxBufSize = maxBufSize;
+		//this.timeout = timeout;	
+		socket = new Socket();
+		//socket.setTimeout( timeout );
+		buf = haxe.io.Bytes.alloc( defaultBufSize );
+		bufbytes = 0;
+		reading = false;
+	}
+	
+	public override function connect() {
+		#if neko
+		socket.connect( Socket.resolve( host ), port );
+		#elseif php
+		socket.connectTLS( new php.net.Host( host ), port );
+		#end
+		connected = true;
+		__onConnect();
+	}
+	
+	public override function disconnect() {
+		if( !connected )
+			return;
+		socket.close();
+		#if (neko||php||cpp)
+		reading = false;
+		#end
+		connected = false;
+	}
+	
+	public override function read( ?yes : Bool = true ) : Bool {
+		reading = true;
+		while( reading ) {
+			var buflen = buf.length;
+			if( bufbytes == buflen ) {
+				var nsize = buflen*2;
+				if( nsize > maxBufSize ) {
+					if( buflen == maxBufSize  )
+						throw "Max buffer size reached ("+maxBufSize+")";
+					nsize = maxBufSize;
+				}
+				
+				var buf2 = haxe.io.Bytes.alloc( nsize );
+				buf2.blit( 0, buf, 0, buflen );
+				buflen = nsize;
+				buf = buf2;
+			}
+			var nbytes = 0;
+			nbytes = socket.input.readBytes( buf, bufbytes, buflen-bufbytes );
+			bufbytes += nbytes;
+			var pos = 0;
+			while( bufbytes > 0 ) {
+				var nbytes = __onData( buf, pos, bufbytes );
+				if( nbytes == 0 )
+					return true;
+				pos += nbytes;
+				bufbytes -= nbytes;
+			}
+			if( reading && pos > 0 )
+				buf.blit( 0, buf, pos, bufbytes );
+		}
+		return true;
+	}
+	
+	public override function write( t : String ) : Bool {
+		socket.write( t );
+		socket.output.flush();
+		return true;
+	}
+	
+}
