25d34fde6c679eefc2a5682829e10e8285d2b986
tong <sdk@disktree.net>
2010-08-14 13:43:07 +0200

    nodejs uses the built-in base64 encoder/decoder now

--- jabber/util/Base64.hx
+++ jabber/util/Base64.hx
@@ -1,7 +1,11 @@
 package jabber.util;
 
+#if nodejs
+import js.Node;
+#end
+
 /**
-	Base64 utility.
+	Base64 encoding/decoding utility.
 */
 class Base64 {
 	
@@ -11,135 +15,61 @@ class Base64 {
 		//'+/='
 		//'-_.'
 		CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-		/*
-		#if js
-		byteToCharMap = new Array();
-		charToByteMap = new Array();
-		// quick mappings back and forth ... precompute two maps.
-		for( i in 0...CHARS.length )
-			charToByteMap[untyped byteToCharMap[i] = CHARS.charAt( i )] = i;
-		#else
-		#end
-		*/
+		#if !nodejs
 		bc = new haxe.BaseCode( haxe.io.Bytes.ofString( CHARS ) );
+		#end
 	}
 	
-	//static var inst : Base64;
-	
 	public static var CHARS(default,null) : String;
+	
+	#if !nodejs
+	
 	public static var bc(default,null) : haxe.BaseCode;
 	
-	/**
-	*/
 	public static function fillNullbits( s : String ) : String {
 		/*
 		var n = (s.length)%3;
 		if( n == 0 ) n -= 1;
 		for( i in 0...n ) s += "=";
+		return s;
 		*/
 		var n = (s.length)%3;
 		n = ( n == 0 ) ? ((s.length-1)%3) : ((s.length)%3+1);
 		for( i in 0...n ) s += "=";
 		return s;
+		
 	}
 	
-	/**
-	*/
 	public static function removeNullbits( s : String ) : String {
 		while( s.charAt( s.length-1 ) == "=" ) s = s.substr( 0, s.length-1 );
 		return s;
 	}
 	
-	/**
-	*/
+	#end
+	
 	public static function encode( t : String ) : String {
+		#if nodejs
+		return Node.newBuffer(t).toString( Node.BASE64 );
+		#else
 		return fillNullbits( bc.encodeString( t ) );
+		#end
 	}
 	
-	/**
-	*/
 	public static function decode( t : String ) : String {
+		#if nodejs
+		return Node.newBuffer( t, Node.BASE64 ).toString( Node.ASCII );
+		#else
 		return bc.decodeString( removeNullbits( t ) );
+		#end
 	}
 	
-	/*
-	#if js
-	
-	static var byteToCharMap : Array<String>;
-	static var charToByteMap : Array<Int>;
-
-	public static function encodeByteArray( t : Array<Int> ) : String {
-		var s = new Array<String>();
-		var i = 0;
-		while( i < t.length ) {
-			var b1 = t[i];
-			var haveB2 = i+1 < t.length;
-			var b2 = haveB2 ? t[i+1] : 0;
-			var haveB3 = i + 2 < t.length;
-			var b3 = haveB3 ? t[i+2] : 0;
-			var ob1 = b1 >> 2;
-			var ob2 = ((b1 & 0x03) << 4) | (b2 >> 4);
-		    var ob3 = ((b2 & 0x0F) << 2) | (b3 >> 6);
-			var ob4 = b3 & 0x3F;
-			if( !haveB3 ) {
-				ob4 = 64;
-				if( !haveB2 ) ob3 = 64;
-			}
-			s.push( byteToCharMap[ob1] );
-			s.push( byteToCharMap[ob2] );
-			s.push( byteToCharMap[ob3] );
-			s.push( byteToCharMap[ob4] );
-			i += 3;
-		}
-		return s.join( '' );
-	}
-	
-	public static function decodeStringToByteArray( t : String ) : Array<Int> {
-		if( t.length % 4 != 0 )
-			throw 'Length of b64-encoded data must be zero mod four';
-		var a = new Array<Int>();
-		var i = 0;
-		while( i < t.length ) {
-			var b1 = untyped charToByteMap[t.charAt(i)];
-			var b2 = untyped charToByteMap[t.charAt(i+1)];
-			var b3 = untyped charToByteMap[t.charAt(i+2)];
-			var b4 = untyped charToByteMap[t.charAt(i+3)];
-			if( b1 == null || b2 == null || b3 == null || b4 == null )
-				throw "Base64 decoding error";
-			a.push( (b1<<2)|(b2>>4) );
-			if( b3 != 64 ) {
-				a.push( ((b2<<4) & 0xF0) | (b3>>2) );
-				if( b4 != 64 )
-					a.push( ((b3<<6) & 0xC0) | b4 );
-			}
-			i += 4;
-		}
-		return a;
-	}
-	
-	#end // js
-	*/
-	
-	//TODO
 	/**
-		Creates a random string of given length.
+		Create a random string of given length.
 	*/
-	public static function random( ?len : Int = 1 ) : String {
-		var b = new StringBuf();
-		var bits = 0;
-		var bitcount = 0;
-		var i = 0;
-		while( i < len ) {
-			//if( bitcount < 6 ) {
-			bits = Std.int( Math.random()*CHARS.length);//Math.POSITIVE_INFINITY);
-				//bitcount = 32;
-			//}
-			b.add( CHARS.charAt(bits&0x3F));
-			bits >>= 6;
-			bitcount -= 6;
-			i++;
-		}
-		return b.toString();
+	public static function random( len : Int = 1, ?chars : String ) : String {
+		if( chars == null ) chars = CHARS;
+		var r = "";
+		for( i in 0...len ) r += chars.substr( Math.floor( Math.random()*chars.length ), 1 );
+		return r;
 	}
-
 }
