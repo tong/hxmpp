011b9b2a4a62403e3122838193ac1c685a754a46
tong <sdk@disktree.net>
2010-10-22 02:21:11 +0200

    systemutil, get os name fix

--- jabber/util/SystemUtil.hx
+++ jabber/util/SystemUtil.hx
@@ -1,63 +1,32 @@
 package jabber.util;
 
+#if neko
+import neko.Sys;
+#elseif cpp
+import cpp.Sys;
+#elseif php
+import php.Sys;
+#elseif (nodejs||rhino)
+import js.Sys;
+#end
+
 class SystemUtil {
 	
 	/**
 		Returns the name of the OS used.
 	*/
 	public static #if !js inline #end function systemName() : String {
-		#if !js
-		return	#if neko neko.Sys.systemName();
-				#elseif php php.Sys.systemName();
-				#elseif flash9 flash.system.Capabilities.os;
-				#end
-		#else
-		var s = js.Lib.window.navigator.appVersion;
-		var os = "Unknown";
-		if( s.indexOf( "Win" ) != -1 ) return "Windows";
-		if( s.indexOf( "Mac" ) != -1 ) return "MacOS";
-		if( s.indexOf( "X11" ) != -1 ) os = "Unix";
-		if( s.indexOf( "Linux" ) != -1 ) os = "Linux";
-		return os;
+		#if (neko||cpp||php||nodejs)
+		return Sys.systemName();
+		#elseif flash
+		return flash.system.Capabilities.os;
+		#elseif js
+		var os = js.Lib.window.navigator.appVersion;
+		return switch( os ) {
+		case "X11","Linux" : "Linux";
+		default : os;
+		}
 		#end
 	}
-	
-		/*
-	public static function getProcInfo() {
-		if( neko.Sys.systemName() != "Linux" ) {
-		}
-		
-		import os
-_proc_status = '/proc/%d/status' % os.getpid( )
-_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
-          'KB': 1024.0, 'MB': 1024.0*1024.0}
-def _VmB(VmKey):
-    ''' given a VmKey string, returns a number of bytes. '''
-    # get pseudo file  /proc/<pid>/status
-    try:
-        t = open(_proc_status)
-        v = t.read( )
-        t.close( )
-    except IOError:
-        return 0.0  # non-Linux?
-    # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
-    i = v.index(VmKey)
-    v = v[i:].split(None, 3)  # split on runs of whitespace
-    if len(v) < 3:
-        return 0.0  # invalid format?
-    # convert Vm value to bytes
-    return float(v[1]) * _scale[v[2]]
-def memory(since=0.0):
-    ''' Return virtual memory usage in bytes. '''
-    return _VmB('VmSize:') - since
-def resident(since=0.0):
-    ''' Return resident memory usage in bytes. '''
-    return _VmB('VmRSS:') - since
-def stacksize(since=0.0):
-    ''' Return stack size in bytes. '''
-    return _VmB('VmStk:') - since
-		
-	}
-		*/
 	
 }
