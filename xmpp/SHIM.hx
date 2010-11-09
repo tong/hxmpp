46a891aac5557db820148d00df7e773d0f72e21b
tong <sdk@disktree.net>
2010-11-09 07:22:38 +0100

    added xmpp SHIM class

---
+++ xmpp/SHIM.hx
@@ -0,0 +1,57 @@
+/*
+ *	This file is part of HXMPP.
+ *	Copyright (c)2009 http://www.disktree.net
+ *	
+ *	HXMPP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  HXMPP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *	See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with HXMPP. If not, see <http://www.gnu.org/licenses/>.
+*/
+package xmpp;
+
+private typedef Header = {
+	var name : String;
+	var value : String;
+}
+
+class SHIM {
+	
+	public static var XMLNS = "http://jabber.org/protocol/shim";
+	
+	public var headers : List<Header>;
+	
+	public function new() {
+		headers = new List();
+	}
+	
+	public function toXml() : Xml {
+		var x = Xml.createElement( "headers" );
+		x.set( "xmlns", XMLNS );
+		for( h in headers ) {
+			var e = XMLUtil.createElement( "header", h.value );
+			e.set( "name", h.name );
+			x.addChild( e );
+		}
+		return x;
+	}
+	
+	public static function parse( x : Xml ) : xmpp.SHIM {
+		var s = new SHIM();
+		for( e in x.elementsNamed( "header" ) ) {
+			s.headers.add( {
+				name : e.get("name"),
+				value : e.firstChild().nodeValue
+			} );
+		}
+		return s;
+	}
+	
+}
