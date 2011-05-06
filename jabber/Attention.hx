2b7b9aacc92c206c20243ac8bee618342119df28
tong <sdk@disktree.net>
2011-05-06 13:06:26 +0200

    added xep224 support ('attention')

---
+++ jabber/Attention.hx
@@ -0,0 +1,38 @@
+/*
+ *	This file is part of HXMPP.
+ *	Copyright (c)2009 http://www.disktree.net
+ *	
+ *	HXMPP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  HXMPP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *	See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with HXMPP. If not, see <http://www.gnu.org/licenses/>.
+*/
+package jabber;
+
+/**
+	<a href="http://www.xmpp.org/extensions/xep-0224.html">XEP 224 - Attention</a><br/>
+	Extension for getting the attention of another user.
+*/
+class Attention {
+	
+	public var stream(default,null) : Stream;
+	
+	public function new( stream : Stream ) {
+		this.stream = stream;
+	}
+	
+	public function capture( jid : String, message : String ) {
+		var m = new xmpp.Message( jid, message );
+		m.properties.push( Xml.parse( "<attention xmlns='urn:xmpp:attention:0'/>" ) );
+		stream.sendPacket( m );
+	}
+	
+}
---
+++ jabber/AttentionListener.hx
@@ -0,0 +1,50 @@
+/*
+ *	This file is part of HXMPP.
+ *	Copyright (c)2009 http://www.disktree.net
+ *	
+ *	HXMPP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  HXMPP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *	See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with HXMPP. If not, see <http://www.gnu.org/licenses/>.
+*/
+package jabber;
+
+import jabber.stream.PacketCollector;
+
+/**
+	<a href="http://www.xmpp.org/extensions/xep-0224.html">XEP 224 - Attention</a><br/>
+	Listens/Reports attention request.
+*/
+class AttentionListener {
+	
+	public dynamic function onCapture( m : xmpp.Message ) : Void;
+	
+	public var stream(default,null) : Stream;
+	
+	var c : PacketCollector;
+	
+	public function new( stream : Stream, ?onCapture : xmpp.Message->Void ) {
+		if( !stream.features.add( 'urn:xmpp:attention:0' ) )
+			throw 'attention listener already added';
+		this.stream = stream;
+		this.onCapture = onCapture;
+		c = stream.collect( [cast new xmpp.filter.MessageFilter(xmpp.MessageType.chat)], handleRequest, true );
+	}
+	
+	public function dispose() {
+		stream.removeCollector( c );
+	}
+	
+	function handleRequest( m : xmpp.Message ) {
+		onCapture( m );
+	}
+
+}
