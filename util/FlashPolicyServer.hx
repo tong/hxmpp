8e9f09007ba48ab40e72f85b66378a61723c1267
tong <sdk@disktree.net>
2011-07-20 10:45:46 +0200

    moved FlashPolicyServer out of jabber package

--- util/FlashPolicyServer.hx
+++ util/FlashPolicyServer.hx
@@ -15,7 +15,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with HXMPP. If not, see <http://www.gnu.org/licenses/>.
 */
-package jabber.util;
+package;
 
 #if nodejs
 import js.Node;
@@ -47,7 +47,7 @@ private typedef AllowedDomain = {
 }
 
 /**
-	A flash policy server (neko,cpp,nodejs).
+	A standalone flash policy server (neko,cpp,nodejs,air).
 */
 class FlashPolicyServer {
 	
