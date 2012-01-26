4ba16489343f7870c8026ef549b556beb1e02133
tong <sdk@disktree.net>
2012-01-26 17:10:22 +0100

    misc sync

--- util/WebSocket.hx
+++ util/WebSocket.hx
@@ -8,16 +8,16 @@ extern class WebSocket {
 	static inline var CLOSING = 2;
 	static inline var CLOSED = 3;
 	
-	var readyState(default,null) : Int;
-	var bufferedAmount(default,null) : Int;
+	var readyState(default,never) : Int;
+	var bufferedAmount(default,never) : Int;
 	
 	dynamic function onopen() : Void;
 	dynamic function onmessage(e:{data:String}) : Void; //correct?
 	dynamic function onclose() : Void;
 	dynamic function onerror() : Void;
 	
-	var url(default,null) : String;
-	var protocol(default,null) : String;
+	var url(default,never) : String;
+	var protocol(default,never) : String;
 	
 	function new( url : String, ?protocol : Dynamic ) : Void;
 	
