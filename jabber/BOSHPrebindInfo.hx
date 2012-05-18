fd965ede8fa6b976c9eec7f1e12d207df25b850d
tong <sdk@disktree.net>
2012-05-18 14:01:29 +0200

    doc

--- jabber/BOSHPrebindInfo.hx
+++ jabber/BOSHPrebindInfo.hx
@@ -4,10 +4,34 @@ package jabber;
 	The info passed to the client aplication for attaching the BOSH connection.
 */
 typedef BOSHPrebindInfo = {
-	serviceUrl : String,
-	jid : String,
-	sid : String,
-	rid : Int,
-	wait : Int,
-	hold : Int
+	
+	/**
+		The URI of the HTTP/BOSH service
+	*/
+	var serviceUrl : String;
+	
+	/**
+		The full JID prebound 
+	*/
+	var jid : String;
+	
+	/**
+		The SID of the BOSH prebound session
+	*/
+	var sid : String;
+	
+	/**
+		The RID of the BOSH prebound session
+	*/
+	var rid : Int;
+	
+	/**
+		The 'wait' value of the BOSH session
+	*/
+	var wait : Int;
+	
+	/**
+		The 'hold' value of the BOSH session
+	*/
+	var hold : Int;
 }
