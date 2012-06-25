9b0ec9f34ff0ebf8a93cfe2f5679f6430df9182c
tong <sdk@disktree.net>
2012-06-25 19:48:17 +0200

    MIT

--- jabber/BOBListener.hx
+++ jabber/BOBListener.hx
@@ -1,57 +1,27 @@
 /*
- *	This file is part of HXMPP.
- *	Copyright (c)2009 http://www.disktree.net
- *	
- *	HXMPP is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  HXMPP is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *	See the GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with HXMPP. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (c) 2012, tong, disktree.net
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package jabber;
 
 import jabber.util.Base64;
-
-/*
-class BOBListener extends jabber.stream.IQListener {
-	
-	public var onRequest : String->String->xmpp.BOB;
-	
-	public function new( stream : jabber.Stream, onRequest : String->String->xmpp.BOB ) {
-		super( stream, xmpp.BOB.XMLNS );
-		this.onRequest = onRequest;
-	}
-	
-	public override function listen( ?filters : Array<PacketFilter>, ?iqType : xmpp.IQType, ?nodeName : String ) : String {
-		super.listen( filters, xmpp.IQType.get, "data" );
-	}
-	
-	override function handleRequest( iq : xmpp.IQ ) {
-		var _bob = xmpp.BOB.parse( iq.x.toXml() );
-		var _cid = xmpp.BOB.getCIDParts( _bob.cid );
-		var bob : xmpp.BOB = onRequest( iq.from, _cid[1] );
-		if( bob == null ) {
-			//trace("NO BOB FOUND");
-			//TODO check XEP for updates
-			//.??
-		} else {
-			var r = new xmpp.IQ( xmpp.IQType.result, iq.id, iq.from );
-			// encode here?
-			bob.data =  new haxe.BaseCode( haxe.io.Bytes.ofString( Base64.CHARS ) ).encodeString( bob.data );
-			r.x = bob;
-			stream.sendPacket( r );
-		}
-	}
-	
-}
-*/
 
 /**
 	Listens for 'Bits Of Binary' requests.
@@ -60,8 +30,7 @@ class BOBListener extends jabber.stream.IQListener {
 class BOBListener {
 	
 	/**
-		Callback handler for BOB requests.<br/>
-		JID->CID .. return BOB
+		Callback handler for BOB requests.
 	*/
 	public var onRequest : String->String->xmpp.BOB;
 	
