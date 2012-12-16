1738b0554ef52243b9c81c6bdf7b707759cc9251
tong <sdk@disktree.net>
2012-12-16 03:05:21 +0100

    moved jabber.WebSocketConnection to jabber.SocketConnection (default for js)

--- jabber/SocketConnection.hx
+++ jabber/SocketConnection.hx
@@ -173,6 +173,75 @@ class SocketConnection extends jabber.stream.Connection {
 }
 
 
+#elseif ( js && !JABBER_FLASHSOCKETBRIDGE && !node && !rhino )
+
+/**
+	WebSocket connection.
+	http://tools.ietf.org/html/draft-moffitt-xmpp-over-websocket-00
+	
+	For flash you have to add some javascript glue code to your website
+*/
+class SocketConnection extends jabber.stream.Connection {
+	
+	public var url(default,null) : String;
+	public var port(default,null) : Int;
+	public var socket(default,null) : WebSocket;
+	
+	public function new( host : String = "localhost", port : Int = 5222, secure : Bool = false ) {
+		
+		super( host, secure );
+		this.port = port;
+		this.secure = secure;
+		
+		url = "ws"+(secure?"s":"")+"://"+host+":"+port; // unofficial specs do not support secure websocket connections (?)
+	}
+	
+	public override function connect() {
+		socket = new WebSocket( url );
+		socket.onopen = onConnect;
+		socket.onclose = onClose;
+		socket.onerror = onError;
+	}
+	
+	public override function disconnect() {
+		socket.close();
+	}
+	
+	public override function read( ?yes : Bool = true ) : Bool {
+		socket.onmessage = yes ? onData : null;
+		return true;
+	}
+	
+	public override function write( t : String ) : Bool {
+		socket.send( t );
+		return true;
+	}
+	
+	function onConnect() {
+		connected = true;
+		__onConnect();
+	}
+	
+	function onClose() {
+		connected = false;
+		__onDisconnect(null);
+	}
+	
+	function onError() {
+		connected = false;
+		__onDisconnect( "websocket error" ); // no error message?
+	}
+	
+	function onData( m ) {
+		__onString( m.data );
+	}
+	
+	public static inline function isSupported() : Bool {
+		return untyped window.WebSocket != null;
+	}
+}
+
+
 #elseif ( flash && TLS )
 
 import flash.utils.ByteArray;
@@ -380,7 +449,7 @@ import air.ProgressEvent;
 import air.SecurityErrorEvent;
 
 /**
-	JS + Air
+	Air/Javascript
 */
 class SocketConnection extends jabber.stream.SocketConnectionBase {
 	
