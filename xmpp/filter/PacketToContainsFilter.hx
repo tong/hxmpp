60fc092b99986407a19055df9ccb9a77f93515d8
tong <sdk@disktree.net>
2013-02-22 09:11:47 +0100

    haxe3 fix

--- xmpp/filter/PacketToContainsFilter.hx
+++ xmpp/filter/PacketToContainsFilter.hx
@@ -25,20 +25,20 @@ package xmpp.filter;
 */
 class PacketToContainsFilter {
 	
-	public var contains(default,setContains) : String;
+	public var contains(default,set) : String;
 	
 	var ereg : EReg;
 	
 	public function new( contains : String ) {
-		setContains( contains );
+		set_contains( contains );
 	}
 	
-	function setContains( t : String ) : String {
+	function set_contains( t : String ) : String {
 		ereg = new EReg( t, "" );
 		return this.contains = t;
 	}
 	
-	@:keep public function accept( p : xmpp.Packet ) : Bool {
+	public function accept( p : xmpp.Packet ) : Bool {
 		return ( p.to == null ) ? false : ereg.match( p.to );
 	}
 	
