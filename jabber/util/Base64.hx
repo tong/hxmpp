ca20933aae298612033519029a40ffa153bd789c
tong <sdk@disktree.net>
2013-05-20 19:56:54 +0200

    base64 nodejs fix

--- jabber/util/Base64.hx
+++ jabber/util/Base64.hx
@@ -94,6 +94,43 @@ class Base64 {
 		#end
 	}
 
+	/**
+	*/
+	public static
+	#if (nodejs||php) inline #end
+	function encodeBytes( b : Bytes ) : Bytes {
+
+		#if php
+		return untyped __call__( "base64_encode", b.getData() );
+		
+		#elseif nodejs
+		return Bytes.ofString( new NodeBuffer( b.toString() ).toString( NodeC.BASE64 ) );
+
+		#else
+		return new BaseCode( Bytes.ofString( CHARS ) ).encodeBytes( b );
+		//return fillNullbits( bc.encodeBytes( b ).toString() );
+		
+		#end
+	}
+
+	/**
+	*/
+	public static
+	#if (nodejs||php) inline #end
+	function decodeBytes( b : Bytes ) : Bytes {
+		
+		#if php
+		return Bytes.ofData( untyped __call__( "base64_decode", b.getData() ) );
+		
+		#elseif nodejs
+		return Bytes.ofString( new NodeBuffer( b.toString(), NodeC.BASE64 ).toString( NodeC.ASCII ) );
+		
+		#else
+		return new BaseCode( Bytes.ofString( CHARS ) ).decodeBytes( b );
+		
+		#end
+	}
+
 	//TODO remove
 	/**
 		Create a random (base64 compatible) string of given length.
@@ -110,137 +147,5 @@ class Base64 {
 			s += chars.charAt( Std.random( n ) );
 		return s;
 	}
-}
-
-
-/*
-class Base64 {
-	
-	public static var CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-	
-	#if (cpp||flash||js||neko||java||cs)
-
-	static var bc = new haxe.crypto.BaseCode( Bytes.ofString( CHARS ) );
-	
-	public static function getNullbits( s : String ) : String {
-	 	return switch( s.length % 3 )  {
-			case 2 : "=";
-			case 1 : "==";
-			default : "";
-		}
-	}
-	
-	public static inline function fillNullbits( s : String ) : String {
-		return s + getNullbits(s);
-	}
-	
-	public static function removeNullbits( s : String ) : String {
-		var p = s.length-1;
-		while( s.charAt( p ) == "=" ) p--;
-		return s.substr( 0, p+1 );
-	}
-	
-	#end
-	
-	public static
-	#if (nodejs||php) inline #end
-	function encode( s : String ) : String {
-		
-		#if nodejs
-		return new NodeBuffer(s).toString( NodeC.BASE64 );
-		//return new Buffer( s, Node.BASE64 ).toString( Node.UTF8 );
-		
-		#elseif php
-		return untyped __call__( "base64_encode", s );
-		
-		#else
-			
-			#if js
-			if(  untyped window.btoa != null ) {
-				return untyped window.btoa( s );
-			}
-	        #end
-
-	        //TODO wtf
-	      	s = removeNullbits( s );
-	        var p = getNullbits(s);
-	        var r = bc.encodeString( s );
-	        return r+p;
-	       // var r = bc.encodeString( s );
-	       // return fillNullbits(r);
-	       
-		#end
-	}
-	
-	public static inline function decode( s : String ) : String {
-		
-		#if nodejs
-		return new NodeBuffer( s, NodeC.BASE64 ).toString( NodeC.ASCII );
-		
-		#elseif php
-		return untyped __call__( "base64_decode", s );
-		
-		#else
-			
-			#if js
-			return if( untyped window.atob != null ) {
-				untyped window.atob( s );
-			} else {
-				bc.decodeString( removeNullbits(s) );
-			}
-
-			#else
-			return bc.decodeString( removeNullbits(s) );
-
-			#end
-		#end
-	}
-	
-	public static inline function encodeBytes( b : Bytes ) : String {
-		
-		#if php
-		return untyped __call__( "base64_encode", b.getData() );
-		
-		#elseif nodejs
-		//TODO
-		var d : Dynamic = b.getData();
-		return d.toString( NodeC.BASE64 );
-		//return new NodeBuffer( b.getData().toString( NodeC.ASCII ) );
-		//return new NodeBuffer( b.getData().toString(), NodeC.BASE64 ).toString( NodeC.ASCII );
-		//return b.getData().toString( NodeC.BASE64 );
-	
-		#else
-		return fillNullbits( bc.encodeBytes( b ).toString() );
-		
-		#end
-	}
-	
-	public static inline function decodeBytes( s : String ) : Bytes {
-		#if php
-		return Bytes.ofString( untyped __call__( "base64_decode", s ) );
-		#elseif nodejs
-		return Bytes.ofString( new NodeBuffer( s ).toString( NodeC.BASE64 ) );
-		#else
-		return bc.decodeBytes( Bytes.ofString( removeNullbits( s ) ) );
-		#end
-	}
-	
-	/**
-		Create a random (base64 compatible) string of given length.
-//TODO remove
-	* /
-	public static function random( len : Int = 1, ?chars : String ) : String {
-		var n : Null<Int> = null;
-		if( chars == null ) {
-			chars = CHARS;
-			n = CHARS.length-2;
-		} else
-			n = chars.length;
-		var s = "";
-		for( i in 0...len )
-			s += chars.charAt( Std.random( n ) );
-		return s;
-	}
 	
 }
-*/
