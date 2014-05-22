098f1967f75346082956ae3b94404287fd65891a
tong <tong@disktree.net>
2014-05-22 14:26:02 +0200

    md5 fix for java/cs

--- src/jabber/util/MD5.hx
+++ src/jabber/util/MD5.hx
@@ -27,22 +27,17 @@ import js.Node;
 
 /*
 	Creates a MD5 of a String.
+
 	Modified version from std to provide raw encoding as well as support for non-official haxe targets.
 */
 class MD5 {
 
 	public static inline function encode( s : String, raw : Bool = false ) : String {
-		
-		//TODO
-		//#if jabber_hxssl_crypto
-		//#if hxssl_crypto
-		//#if hxssl
+
+		//#if (hxssl&&hxssl_crypto)
 		//return sys.crypto.MD5.encode( s, raw );
 
-		#if cpp
-		return raw ? new MD5().doEncodeRaw(s) : new MD5().doEncode(s);
-
-		#elseif js
+		#if js
 			#if nodejs
 			var h = Node.crypto.createHash( "md5" );
 			h.update( s );
@@ -50,33 +45,40 @@ class MD5 {
 			#else
 			return raw ? new MD5().doEncodeRaw(s) : new MD5().doEncode(s);
 			#end
-			
-		#elseif neko
-		var t = make_md5( untyped s.__s );
-		return untyped new String( raw ? t : base_encode( t, "0123456789abcdef".__s ) );
+
+		#elseif (java||cs)
+		return raw ? new MD5().doEncodeRaw(s) : new MD5().doEncode(s);
 		
-		#elseif php
-		return untyped __call__( "md5", s, raw );
-
 		#else
-		return raw ? haxe.crypto.Md5.make(haxe.io.Bytes.ofString(s)).toString() : haxe.crypto.Md5.encode(s);
-
+		return raw ? haxe.crypto.Md5.make( haxe.io.Bytes.ofString(s) ).toString() : haxe.crypto.Md5.encode(s);
+		
 		#end
+
+
 	}
 
-	#if neko
-	static var base_encode = neko.Lib.load( "std", "base_encode", 2 );
-	static var make_md5 = neko.Lib.load( "std", "make_md5", 1 );
-	#elseif (cpp||js)
-	
-	static var inst = new MD5();
-	
+	#if (cs||java||(js&&!nodejs))
+
 	function new() {}
+
+	function doEncodeRaw( t : String ) : String {
+		var len = t.length*8;
+		var x = str2bin( t );
+		x[len >> 5] |= 0x80 << ((len) % 32);
+		x[(((len + 64) >>> 9) << 4) + 14] = len;
+		return bin2str( _encode( x ) );
+	}
+	
+	function doEncode( t : String ) : String {
+		var t = _encode( str2blks(t) );
+		return rhex(t[0])+rhex(t[1])+rhex(t[2])+rhex(t[3]);
+	}
 	
 	function rhex( n : Int ) : String {
 		var s = "";
 		var hex = "0123456789abcdef";
-		for( j in 0...4 ) s += hex.charAt( (n >> (j * 8 + 4)) & 0x0F ) + hex.charAt( (n >> (j * 8)) & 0x0F );
+		for( j in 0...4 )
+			s += hex.charAt( (n >> (j * 8 + 4)) & 0x0F ) + hex.charAt( (n >> (j * 8)) & 0x0F );
 		return s;
 	}
 	
@@ -152,8 +154,8 @@ class MD5 {
 		for( i in 0...r.length ) r[i] = 0;
 		var i2 = 0;
 		while( i2 < inp.length * 8 ) {
-		    r[i2>>5] |=  ( StringTools.fastCodeAt( inp, Std.int( i2 / 8 ) ) & 0xFF) << ( i2 % 32 );
-		    i2 += 8;
+			r[i2>>5] |=  ( StringTools.fastCodeAt( inp, Std.int( i2 / 8 ) ) & 0xFF) << ( i2 % 32 );
+			i2 += 8;
 		}
 		return r;
 	}
@@ -168,19 +170,6 @@ class MD5 {
 		return r;
 	}
 	
-	function doEncodeRaw( t : String ) : String {
-		var len = t.length*8;
-		var x = str2bin( t );
-		x[len >> 5] |= 0x80 << ((len) % 32);
-		x[(((len + 64) >>> 9) << 4) + 14] = len;
-		return bin2str( _encode( x ) );
-	}
-	
-	function doEncode( t : String ) : String {
-		var t = _encode( str2blks(t) );
-		return rhex(t[0])+rhex(t[1])+rhex(t[2])+rhex(t[3]);
-	}
-	
 	function _encode( x : Array<Int> ) : Array<Int> {
 		
 		var a : Int =  1732584193;
@@ -191,85 +180,85 @@ class MD5 {
 		var i = 0;
 		while( i < x.length ) {
 			
-		    var olda : Int = a;
-		    var oldb : Int = b;
-		    var oldc : Int = c;
-		    var oldd : Int = d;
-		    
-		    a = ff(a, b, c, d, x[i+ 0], 7 , -680876936);
-		    d = ff(d, a, b, c, x[i+ 1], 12, -389564586);
-		    c = ff(c, d, a, b, x[i+ 2], 17,  606105819);
-		    b = ff(b, c, d, a, x[i+ 3], 22, -1044525330);
-		    a = ff(a, b, c, d, x[i+ 4], 7 , -176418897);
-		    d = ff(d, a, b, c, x[i+ 5], 12,  1200080426);
-		    c = ff(c, d, a, b, x[i+ 6], 17, -1473231341);
-		    b = ff(b, c, d, a, x[i+ 7], 22, -45705983);
-		    a = ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
-		    d = ff(d, a, b, c, x[i+ 9], 12, -1958414417);
-		    c = ff(c, d, a, b, x[i+10], 17, -42063);
-		    b = ff(b, c, d, a, x[i+11], 22, -1990404162);
-		    a = ff(a, b, c, d, x[i+12], 7 ,  1804603682);
-		    d = ff(d, a, b, c, x[i+13], 12, -40341101);
-		    c = ff(c, d, a, b, x[i+14], 17, -1502002290);
-		    b = ff(b, c, d, a, x[i+15], 22,  1236535329);
-		
-		    a = gg(a, b, c, d, x[i+ 1], 5 , -165796510);
-		    d = gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
-		    c = gg(c, d, a, b, x[i+11], 14,  643717713);
-		    b = gg(b, c, d, a, x[i+ 0], 20, -373897302);
-		    a = gg(a, b, c, d, x[i+ 5], 5 , -701558691);
-		    d = gg(d, a, b, c, x[i+10], 9 ,  38016083);
-		    c = gg(c, d, a, b, x[i+15], 14, -660478335);
-		    b = gg(b, c, d, a, x[i+ 4], 20, -405537848);
-		    a = gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
-		    d = gg(d, a, b, c, x[i+14], 9 , -1019803690);
-		    c = gg(c, d, a, b, x[i+ 3], 14, -187363961);
-		    b = gg(b, c, d, a, x[i+ 8], 20,  1163531501);
-		    a = gg(a, b, c, d, x[i+13], 5 , -1444681467);
-		    d = gg(d, a, b, c, x[i+ 2], 9 , -51403784);
-		    c = gg(c, d, a, b, x[i+ 7], 14,  1735328473);
-		    b = gg(b, c, d, a, x[i+12], 20, -1926607734);
-		
-		    a = hh(a, b, c, d, x[i+ 5], 4 , -378558);
-		    d = hh(d, a, b, c, x[i+ 8], 11, -2022574463);
-		    c = hh(c, d, a, b, x[i+11], 16,  1839030562);
-		    b = hh(b, c, d, a, x[i+14], 23, -35309556);
-		    a = hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
-		    d = hh(d, a, b, c, x[i+ 4], 11,  1272893353);
-		    c = hh(c, d, a, b, x[i+ 7], 16, -155497632);
-		    b = hh(b, c, d, a, x[i+10], 23, -1094730640);
-		    a = hh(a, b, c, d, x[i+13], 4 ,  681279174);
-		    d = hh(d, a, b, c, x[i+ 0], 11, -358537222);
-		    c = hh(c, d, a, b, x[i+ 3], 16, -722521979);
-		    b = hh(b, c, d, a, x[i+ 6], 23,  76029189);
-		    a = hh(a, b, c, d, x[i+ 9], 4 , -640364487);
-		    d = hh(d, a, b, c, x[i+12], 11, -421815835);
-		    c = hh(c, d, a, b, x[i+15], 16,  530742520);
-		    b = hh(b, c, d, a, x[i+ 2], 23, -995338651);
-		
-		    a = ii(a, b, c, d, x[i+ 0], 6 , -198630844);
-		    d = ii(d, a, b, c, x[i+ 7], 10,  1126891415);
-		    c = ii(c, d, a, b, x[i+14], 15, -1416354905);
-		    b = ii(b, c, d, a, x[i+ 5], 21, -57434055);
-		    a = ii(a, b, c, d, x[i+12], 6 ,  1700485571);
-		    d = ii(d, a, b, c, x[i+ 3], 10, -1894986606);
-		    c = ii(c, d, a, b, x[i+10], 15, -1051523);
-		    b = ii(b, c, d, a, x[i+ 1], 21, -2054922799);
-		    a = ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
-		    d = ii(d, a, b, c, x[i+15], 10, -30611744);
-		    c = ii(c, d, a, b, x[i+ 6], 15, -1560198380);
-		    b = ii(b, c, d, a, x[i+13], 21,  1309151649);
-		    a = ii(a, b, c, d, x[i+ 4], 6 , -145523070);
-		    d = ii(d, a, b, c, x[i+11], 10, -1120210379);
-		    c = ii(c, d, a, b, x[i+ 2], 15,  718787259);
-		    b = ii(b, c, d, a, x[i+ 9], 21, -343485551);
-		
-		    a = addme( a, olda );
-		    b = addme( b, oldb );
-		    c = addme( c, oldc );
-		    d = addme( d, oldd );
-		    
-		    i += 16;
+			var olda : Int = a;
+			var oldb : Int = b;
+			var oldc : Int = c;
+			var oldd : Int = d;
+			
+			a = ff(a, b, c, d, x[i+ 0], 7 , -680876936);
+			d = ff(d, a, b, c, x[i+ 1], 12, -389564586);
+			c = ff(c, d, a, b, x[i+ 2], 17,  606105819);
+			b = ff(b, c, d, a, x[i+ 3], 22, -1044525330);
+			a = ff(a, b, c, d, x[i+ 4], 7 , -176418897);
+			d = ff(d, a, b, c, x[i+ 5], 12,  1200080426);
+			c = ff(c, d, a, b, x[i+ 6], 17, -1473231341);
+			b = ff(b, c, d, a, x[i+ 7], 22, -45705983);
+			a = ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
+			d = ff(d, a, b, c, x[i+ 9], 12, -1958414417);
+			c = ff(c, d, a, b, x[i+10], 17, -42063);
+			b = ff(b, c, d, a, x[i+11], 22, -1990404162);
+			a = ff(a, b, c, d, x[i+12], 7 ,  1804603682);
+			d = ff(d, a, b, c, x[i+13], 12, -40341101);
+			c = ff(c, d, a, b, x[i+14], 17, -1502002290);
+			b = ff(b, c, d, a, x[i+15], 22,  1236535329);
+			
+			a = gg(a, b, c, d, x[i+ 1], 5 , -165796510);
+			d = gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
+			c = gg(c, d, a, b, x[i+11], 14,  643717713);
+			b = gg(b, c, d, a, x[i+ 0], 20, -373897302);
+			a = gg(a, b, c, d, x[i+ 5], 5 , -701558691);
+			d = gg(d, a, b, c, x[i+10], 9 ,  38016083);
+			c = gg(c, d, a, b, x[i+15], 14, -660478335);
+			b = gg(b, c, d, a, x[i+ 4], 20, -405537848);
+			a = gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
+			d = gg(d, a, b, c, x[i+14], 9 , -1019803690);
+			c = gg(c, d, a, b, x[i+ 3], 14, -187363961);
+			b = gg(b, c, d, a, x[i+ 8], 20,  1163531501);
+			a = gg(a, b, c, d, x[i+13], 5 , -1444681467);
+			d = gg(d, a, b, c, x[i+ 2], 9 , -51403784);
+			c = gg(c, d, a, b, x[i+ 7], 14,  1735328473);
+			b = gg(b, c, d, a, x[i+12], 20, -1926607734);
+			
+			a = hh(a, b, c, d, x[i+ 5], 4 , -378558);
+			d = hh(d, a, b, c, x[i+ 8], 11, -2022574463);
+			c = hh(c, d, a, b, x[i+11], 16,  1839030562);
+			b = hh(b, c, d, a, x[i+14], 23, -35309556);
+			a = hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
+			d = hh(d, a, b, c, x[i+ 4], 11,  1272893353);
+			c = hh(c, d, a, b, x[i+ 7], 16, -155497632);
+			b = hh(b, c, d, a, x[i+10], 23, -1094730640);
+			a = hh(a, b, c, d, x[i+13], 4 ,  681279174);
+			d = hh(d, a, b, c, x[i+ 0], 11, -358537222);
+			c = hh(c, d, a, b, x[i+ 3], 16, -722521979);
+			b = hh(b, c, d, a, x[i+ 6], 23,  76029189);
+			a = hh(a, b, c, d, x[i+ 9], 4 , -640364487);
+			d = hh(d, a, b, c, x[i+12], 11, -421815835);
+			c = hh(c, d, a, b, x[i+15], 16,  530742520);
+			b = hh(b, c, d, a, x[i+ 2], 23, -995338651);
+			
+			a = ii(a, b, c, d, x[i+ 0], 6 , -198630844);
+			d = ii(d, a, b, c, x[i+ 7], 10,  1126891415);
+			c = ii(c, d, a, b, x[i+14], 15, -1416354905);
+			b = ii(b, c, d, a, x[i+ 5], 21, -57434055);
+			a = ii(a, b, c, d, x[i+12], 6 ,  1700485571);
+			d = ii(d, a, b, c, x[i+ 3], 10, -1894986606);
+			c = ii(c, d, a, b, x[i+10], 15, -1051523);
+			b = ii(b, c, d, a, x[i+ 1], 21, -2054922799);
+			a = ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
+			d = ii(d, a, b, c, x[i+15], 10, -30611744);
+			c = ii(c, d, a, b, x[i+ 6], 15, -1560198380);
+			b = ii(b, c, d, a, x[i+13], 21,  1309151649);
+			a = ii(a, b, c, d, x[i+ 4], 6 , -145523070);
+			d = ii(d, a, b, c, x[i+11], 10, -1120210379);
+			c = ii(c, d, a, b, x[i+ 2], 15,  718787259);
+			b = ii(b, c, d, a, x[i+ 9], 21, -343485551);
+			
+			a = addme( a, olda );
+			b = addme( b, oldb );
+			c = addme( c, oldc );
+			d = addme( d, oldd );
+			
+			i += 16;
 		}
 		return [a,b,c,d];
 	}
