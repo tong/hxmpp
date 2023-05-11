616ee1897c5b25ab765e05c4227c4e870a5f9c2c
tong <tong@disktree.net>
2023-05-11 14:55:37 +0200

    Cleanup

--- src/xmpp/client/Authentication.hx
+++ src/xmpp/client/Authentication.hx
@@ -2,8 +2,8 @@ package xmpp.client;
 
 import haxe.crypto.Base64;
 import haxe.io.Bytes;
-import xmpp.Stream.Response;
 import sasl.Mechanism;
+import xmpp.Response;
 
 /*
 	abstract Auth(xmpp.XML) from XML to XML {
@@ -39,69 +39,6 @@ class Authentication {
 	public static inline var XMLNS_BIND = 'urn:ietf:params:xml:ns:xmpp-bind';
 	public static inline var XMLNS_SESSION = 'urn:ietf:params:xml:ns:xmpp-session';
 
-	/*
-		public static function authenticate( stream : Stream, node : String, resource : String, ?password : String, mechanism : Mechanism, callback : (?error:xmpp.Stanza.Error)->Void, ?streamStart : (XML->Void)->Void ) {
-			var _input = stream.input;
-			function _callback(?e) {
-				stream.input = _input;
-				callback( e );
-			}
-			stream.input = function(str) {
-				var xml = XML.parse( str );
-				switch xml.name {
-				case 'challenge':
-					var res = mechanism.createChallengeResponse( xml.text );
-					res = Base64.encode( Bytes.ofString( res ) );
-					stream.send( XML.create( 'response', res ).set( 'xmlns', XMLNS ) );
-				case 'success':
-					trace("################################>");
-					if( streamStart == null ) streamStart = stream.start;
-					streamStart( features -> {
-						//TODO
-						/* trace(features);
-						var bindSupport : Bool = null;
-						var sessionSupport : Bool = null;
-						for( e in features ) {
-							var ns = e.get( 'xmlns' );
-							switch ns {
-							case 'urn:ietf:params:xml:ns:xmpp-bind': bindSupport = true;
-							case 'urn:ietf:params:xml:ns:xmpp-session': sessionSupport = true;
-							}
-						}
-						if( !bindSupport )
-							onResult();
-						 * /
-
-						bindResource( stream, resource, function(?e){
-							if( e != null ) _callback(e) else initSesssion( stream, _callback );
-						});
-
-						 /*
-						stream.set( XML.create( 'bind' ).set( 'xmlns', XMLNS_BIND ).append( XML.create( 'resource', resource ) ), function(res) {
-							switch res {
-							case Result(r):
-								var xml = XML.create( 'session' ).set( 'xmlns', XMLNS_SESSION );
-								stream.set( xml, function(res) {
-									callback( switch res {
-										case Error(e): e;
-										default: null;
-									});
-								});
-							case Error(e): callback( e );
-							}
-						});
-						* /
-					});
-				case 'failure':
-					callback( new xmpp.Stanza.Error( null, xml.elements[0].name, xml.elements[1].text ) );
-				}
-			};
-			var text = mechanism.createAuthenticationText( node, stream.domain, password );
-			var auth = XML.create( 'auth' ).set( 'xmlns', XMLNS ).set( 'mechanism', mechanism.name );
-			if( text != null ) auth.text = Base64.encode( Bytes.ofString( text ), true );
-			stream.send( auth );
-		}
-	 */
 	public static function authenticate(stream:Stream, node:String, resource:String, ?password:String, mechanism:Mechanism,
 			callback:(?error:xmpp.Stanza.Error) -> Void, ?streamStart:(XML->Void)->Void) {
 		saslAuthentication(stream, node, resource, password, mechanism, function(?e, features) {
