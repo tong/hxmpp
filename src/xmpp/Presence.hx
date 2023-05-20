5e5f37263c0c90c01e18a91807bb8bc58c880c09
tong <tong@disktree.net>
2023-05-20 14:15:39 +0200

    Doc

--- src/xmpp/Presence.hx
+++ src/xmpp/Presence.hx
@@ -3,21 +3,22 @@ package xmpp;
 using xmpp.Stanza;
 
 /**
-	Optional <show/> element specifying the particular availability sub-state of an entity or a specific resource thereof.
+	Optional `<show/>` element specifying the particular availability sub-state of an entity or a specific resource thereof.
 
-	See: http://xmpp.org/rfcs/rfc6121.html#presence-syntax-children-show
- */
+	See: https://xmpp.org/rfcs/rfc6121.html#presence-syntax-children-show
+**/
 enum abstract Show(String) to String {
-	/** Especially socialable */
+
+	/** Especially socialable **/
 	var chat;
 
-	/** Away from device */
+	/** Away from device **/
 	var away;
 
-	/** Extended Away */
+	/** Extended Away **/
 	var xa;
 
-	/** Busy */
+	/** Busy **/
 	var dnd;
 
 	@:from public static inline function fromString(s:String)
@@ -31,35 +32,36 @@ enum abstract Show(String) to String {
 }
 
 /**
-	The absence of a 'type' attribute signals that the relevant entity is available for communication.
-	A 'type' attribute with a value of "unavailable" signals that the relevant entity is not available for communication.
+	The absence of a `type` attribute signals that the relevant entity is available for communication.
+	A `type` attribute with a value of *unavailable* signals that the relevant entity is not available for communication.
 
 	Note:
-	 - There is no default value for the 'type' attribute of the <presence/> element.
-	 - There is no value of "available" for the 'type' attribute of the <presence/> element.
+	 - There is no default value for the `type` attribute of the `<presence/>` element.
+	 - There is no value of *available* for the `type` attribute of the `<presence/>` element.
 
-	See: http://xmpp.org/rfcs/rfc6121.html#presence-syntax-type
- */
+	See: https://xmpp.org/rfcs/rfc6121.html#presence-syntax-type
+**/
 enum abstract PresenceType(String) to String {
-	/** An error has occurred regarding processing or delivery of a previously-sent presence stanza. */
+
+	/** An error has occurred regarding processing or delivery of a previously-sent presence stanza. **/
 	var error;
 
-	/** A request for an entity's current presence; SHOULD be generated only by a server on behalf of a user. */
+	/** A request for an entity's current presence; SHOULD be generated only by a server on behalf of a user. **/
 	var probe;
 
-	/** The sender wishes to subscribe to the recipient's presence. */
+	/** The sender wishes to subscribe to the recipient's presence. **/
 	var subscribe;
 
-	/** The sender has allowed the recipient to receive their presence. */
+	/** The sender has allowed the recipient to receive their presence. **/
 	var subscribed;
 
-	/** Signals that the entity is no longer available for communication. */
+	/** Signals that the entity is no longer available for communication. **/
 	var unavailable;
 
-	/** The sender is unsubscribing from another entity's presence. */
+	/** The sender is unsubscribing from another entity's presence. **/
 	var unsubscribe;
 
-	/** The subscription request has been denied or a previously-granted subscription has been cancelled. */
+	/** The subscription request has been denied or a previously-granted subscription has been cancelled. **/
 	var unsubscribed;
 
 	@:from public static inline function fromString(s:String) : PresenceType {
@@ -79,9 +81,10 @@ enum abstract PresenceType(String) to String {
 	Optional `<status/>` element containing human-readable XML character data specifying a natural-language description of an entity's availability.
 	It is normally used in conjunction with the show element to provide a detailed description of an availability state (e.g., "In a meeting") when the presence stanza has no `type` attribute.
 
-	See: http://xmpp.org/rfcs/rfc6121.html#presence-syntax-children-status
+	See: https://xmpp.org/rfcs/rfc6121.html#presence-syntax-children-status
  */
 abstract Status(String) from String to String {
+
 	@:noCompletion public inline function new(s:String)
 		this = s;
 
@@ -96,9 +99,10 @@ abstract Status(String) from String to String {
 	Optional `<priority/>` element containing non-human-readable XML character data that specifies the priority level of the resource.
 	The value MUST be an integer between `-128` and `+127`.
 
-	http://xmpp.org/rfcs/rfc6121.html#presence-syntax-children-priority
+	https://xmpp.org/rfcs/rfc6121.html#presence-syntax-children-priority
  */
 abstract Priority(Int) from Int to Int {
+
 	public static inline var MIN = -128;
 	public static inline var MAX = 127;
 
