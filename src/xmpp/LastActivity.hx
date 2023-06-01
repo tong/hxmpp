febeddee394c915bbcc7174556d1fb77fe879152
tong <tong@disktree.net>
2023-06-01 15:17:32 +0200

    Add last-activity support

---
+++ src/xmpp/LastActivity.hx
@@ -0,0 +1,15 @@
+package xmpp;
+
+/**
+    Communicate information about the last activity associated with an XMPP entity.
+
+    [XEP-0012: Last Activiy](https://xmpp.org/extensions/xep-0012.html)
+**/
+@xep(12)
+class LastActivity {
+
+	public static inline var XMLNS = "jabber:iq:last";
+
+    public static inline function getLastActivity(stream: Stream, ?jid: String, handler: Response<XML>->Void)
+        stream.get(XMLNS, jid ?? stream.domain, handler);
+}
