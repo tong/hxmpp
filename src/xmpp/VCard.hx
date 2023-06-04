c36033982f850b187ff8540f643e1a6165cacfe9
tong <tong@disktree.net>
2023-06-04 06:40:51 +0200

    Add vcard support [XEP-0054]

---
+++ src/xmpp/VCard.hx
@@ -0,0 +1,20 @@
+package xmpp;
+
+import xmpp.IQ;
+
+/**
+    vcard-temp
+
+    [XEP-0054: vcard-temp](https://xmpp.org/extensions/xep-0054.html)
+**/
+@xep(54)
+class VCard {
+
+    public static inline var XMLNS = "vcard-temp";
+
+    public static inline function getVCard(stream: Stream, ?jid: String, handler: Response<XML>->Void) : IQ
+        return stream.get(Payload.create(XMLNS, "vCard"), jid, handler); 
+
+    public static inline function setVCard(stream: Stream, vcard: XML, handler: Response<XML>->Void) : IQ
+        return stream.set(Payload.create(XMLNS, "vCard").append(vcard), handler); 
+}
