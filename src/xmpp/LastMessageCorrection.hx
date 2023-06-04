a077eb469140a15f217d472207e261fbd5f72330
tong <tong@disktree.net>
2023-06-04 17:04:50 +0200

    Add last message correction support [XEP-0308]

---
+++ src/xmpp/LastMessageCorrection.hx
@@ -0,0 +1,28 @@
+package xmpp;
+
+/**
+    Method for indicating that a message is a correction of the last sent message.
+
+    [XEP-0308: Last Message Correction](https://xmpp.org/extensions/xep-0308.html)
+**/
+@xep(308)
+class LastMessageCorrection {
+
+	public static inline var XMLNS = "urn:xmpp:message-correct:0";
+
+	/**
+        Add a message correction property to the given message stanza.
+	**/
+	public static inline function correct(message : xmpp.Message, id: String) : xmpp.Message {
+        message.properties.push(XML.create("replace").set('xmlns', XMLNS));
+        return message;
+	}
+
+	/**
+	   Get the correction id of the given message stanze.
+	**/
+    public static inline function correction(message : xmpp.Message) : String {
+        return message.properties.filter(e -> return e.is(XMLNS))[0].get('id');
+	}
+
+}
