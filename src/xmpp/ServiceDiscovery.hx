24b4687bbeb914b719c4f1964298d4834a3db34b
tong <tong@disktree.net>
2023-07-06 18:12:50 +0200

    Fix

--- src/xmpp/ServiceDiscovery.hx
+++ src/xmpp/ServiceDiscovery.hx
@@ -16,16 +16,16 @@ class ServiceDiscovery {
     /**
         Discover the identity and capabilities of an entity, including the protocols and features it supports.
     **/
-    public static inline function discoInfo(stream: Stream, ?jid: Jid, ?node: String, handler: Response<XML>->Void) : IQ
+    public static inline function discoInfo(stream: Stream, ?jid: String, ?node: String, handler: Response<XML>->Void) : IQ
         return disco(stream, XMLNS_INFO, jid, node, handler);
 
     /**
         Discover the items associated with an entity, such as the list of rooms hosted at a multi-user chat service.
     **/
-    public static inline function discoItems(stream: Stream, ?jid: Jid, ?node: String, handler: Response<Payload>->Void): IQ
+    public static inline function discoItems(stream: Stream, ?jid: String, ?node: String, handler: Response<Payload>->Void): IQ
         return disco(stream, XMLNS_ITEMS, jid, node, handler);
 
-    static function disco(stream: Stream, xmlns: String, ?jid: Jid, ?node: String, handler: Response<Payload>->Void) : IQ {
+    static function disco(stream: Stream, xmlns: String, ?jid: String, ?node: String, handler: Response<Payload>->Void) : IQ {
         final xml = Payload.create(xmlns);
         if(node != null) xml.set("node", node);
         return stream.get(xml, jid ?? stream.domain, handler);
